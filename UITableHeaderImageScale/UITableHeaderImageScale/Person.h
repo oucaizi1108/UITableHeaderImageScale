a79ab7f4ce3c70194d386020eb02e9edbc1396da
yangxiyu1108 <oucaizi1108>
2018-04-26 13:28:48 +0800

    ssp

---
+++ UITableHeaderImageScale/UITableHeaderImageScale/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  UITableHeaderImageScale
+//
+//  Created by oucaizi on 2018/4/26.
+//  Copyright © 2018年 oucaizi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ UITableHeaderImageScale/UITableHeaderImageScale/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  UITableHeaderImageScale
+//
+//  Created by oucaizi on 2018/4/26.
+//  Copyright © 2018年 oucaizi. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
